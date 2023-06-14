8128fabcebf767e900fb3c3c2094f71412a9e7ce
Vladimir Hanhur <aqgz@email.cz>
2023-06-14 09:11:07 +0200

    update js

--- src/js/photo.js
+++ src/js/photo.js
@@ -46,4 +46,34 @@ async function fetchPhotos(searchQueryResult) {
     return results;
 };
 
-export {fetchPhotos};
+// render photos function, make html markup
+
+async function renderedPhotos(results) {
+
+    const { hits } = results;
+
+    markupData.markup = hits.map((hit) =>
+        `<a href="${hit.largeImageURL}"><div class="photo-card">
+        <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy"
+          class="img-item" />
+        <div class="info">
+    <p class="info-item">
+      <b>Likes:</b>${hit.likes}
+    </p>
+    <p class="info-item">
+      <b>Views:</b>${hit.views}
+    </p>
+    <p class="info-item">
+      <b>Comments:</b>${hit.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads:</b>${hit.downloads}
+    </p>
+  </div>
+</div></a>`).join("");
+    
+    return markupData.markup;
+    
+};
+
+export {fetchPhotos, renderedPhotos};
