7c30473900775a269f95a1f7420140bf699cb070
Vladimir Hanhur <aqgz@email.cz>
2023-06-14 07:50:46 +0200

    update js

--- src/index.js
+++ src/index.js
@@ -36,49 +36,6 @@ const searchForm = document.querySelector(".search-form");
 const gallerySelector = document.querySelector(".gallery");
 const btnLoadMore = document.querySelector(".load-more");
 
-btnLoadMore.addEventListener("click", async () => {
-    pageN += 1;
-    pixabayAPI.page = `${pageN}`;
-
-    try 
-    {
-        const results = await fetchPhotos(searchQueryResult);
-        markupData.htmlCode = await renderedPhotos(results);
-
-        gallerySelector.insertAdjacentHTML("beforeend", markupData.htmlCode);
-        btnLoadMore.classList.add("is-visible");
-
-        // simpleLightbox gallery destroys and reinitilized
-        gallery.refresh();
-
-        const {
-            baseUrl,
-            key,
-            image_type,
-            orientation,
-            safesearch,
-            order,
-            page,
-            per_page
-        } = pixabayAPI;
-        const { total, totalHits, hits } = results;
-        const totalPages = Math.ceil(totalHits / per_page);
-
-        if (page >= totalPages) 
-        {
-            btnLoadMore.classList.remove("is-visible");
-        }
-
-        console.log("results", results);
-    } 
-    catch (error) 
-    {
-        Notify.failure("We're sorry, but you've reached the end of search results.");
-    }
-    console.log("btnLoadMore working");
-    console.log("");
-});
-
 // event listener search form
 
 searchForm.addEventListener("submit", async e => {
@@ -158,5 +115,48 @@ searchForm.addEventListener("submit", async e => {
     {
         Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     }
+    console.log("");
+});
+
+btnLoadMore.addEventListener("click", async () => {
+    pageN += 1;
+    pixabayAPI.page = `${pageN}`;
+
+    try 
+    {
+        const results = await fetchPhotos(searchQueryResult);
+        markupData.htmlCode = await renderedPhotos(results);
+
+        gallerySelector.insertAdjacentHTML("beforeend", markupData.htmlCode);
+        btnLoadMore.classList.add("is-visible");
+
+        // simpleLightbox gallery destroys and reinitilized
+        gallery.refresh();
+
+        const {
+            baseUrl,
+            key,
+            image_type,
+            orientation,
+            safesearch,
+            order,
+            page,
+            per_page
+        } = pixabayAPI;
+        const { total, totalHits, hits } = results;
+        const totalPages = Math.ceil(totalHits / per_page);
+
+        if (page >= totalPages) 
+        {
+            btnLoadMore.classList.remove("is-visible");
+        }
+
+        console.log("results", results);
+    } 
+    catch (error) 
+    {
+        Notify.failure("We're sorry, but you've reached the end of search results.");
+    }
+    console.log("btnLoadMore working");
     console.log("");
 });
