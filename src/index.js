94aec90467c5d16f2620ab242b1de945b3fde0d8
Vladimir Hanhur <aqgz@email.cz>
2023-06-14 09:37:02 +0200

    update js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './sass/index.scss';
 import { Notify } from 'notiflix';
 const axios = require('axios').default;
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { searchForm, btnLoadMore } from './js/form';
+import { btnLoadMore } from './js/form';
 
 // -------------------------------------------------------------------------------------------------------
 const pixabayAPI = {
@@ -35,6 +36,98 @@ const btnLoadMore = document.querySelector('.load-more');
 // -------------------------------------------------------------------------------------------------------
 // event listener search form
 
+
+let searchQueryResult = '';
+let q = '';
+let pageN = 1;
+let gallery = new SimpleLightbox('.gallery a', {enableKeyboard: true, });
+
+
+searchForm.addEventListener("submit", async (e) => {
+
+e.preventDefault();
+
+const { elements: { searchQuery } } = e.target;
+
+searchQueryResult = searchQuery.value;
+
+
+// console.log
+console.log("searchQueryResult:",`"${searchQueryResult}"`);
+console.log("q:", `"${q}"`);
+
+if (searchQueryResult === '') {
+    console.log(searchQueryResult);
+    gallerySelector.innerHTML = "";
+    btnLoadMore.classList.remove("is-visible");
+    
+    return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        
+};
+
+if (searchQueryResult !== q) {
+
+    console.log("CHANGED!!! NOT EMPTY QUERY");
+
+    pageN = 1;
+    pixabayAPI.page = `${pageN}`;
+
+    gallerySelector.innerHTML = "";
+    btnLoadMore.classList.remove("is-visible");
+
+} else {
+
+    console.log("page+1!!!");
+
+    pageN += 1;
+    pixabayAPI.page = `${pageN}`;
+    
+    btnLoadMore.classList.remove("is-visible");
+
+};
+
+q = searchQueryResult;
+
+try {
+
+    const results = await fetchPhotos(searchQueryResult);
+    markupData.htmlCode = await renderedPhotos(results);
+
+    gallerySelector.insertAdjacentHTML("beforeend", markupData.htmlCode);
+    btnLoadMore.classList.add("is-visible");
+    
+    // simpleLightbox gallery destroys and reinitilized
+    gallery.refresh();
+    
+    
+
+    const { baseUrl, key, image_type, orientation, safesearch, order, page, per_page } = pixabayAPI;
+    const { total, totalHits, hits } = results;    
+    const totalPages = Math.ceil(totalHits / per_page);
+    
+    
+    if (page >= totalPages) {
+    
+    btnLoadMore.classList.remove("is-visible");
+
+    };
+
+    Notify.success(`'Hooray! We found ${results.totalHits} images.'`);
+
+    //console.log
+    console.log("results", results);
+
+}
+
+catch (error) {
+
+    Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+
+};
+
+// console.log
+console.log("");
+});
 
 // -------------------------------------------------------------------------------------------------------
 // fetch photos function
